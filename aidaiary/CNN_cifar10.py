216b87e358c943581872c222e71cfec4919d2c8b
fuchami <famichiki.yuuki@gmail.com>
2019-04-15 15:12:52 +0900

    fixbug

--- aidaiary/CNN_cifar10.py
+++ aidaiary/CNN_cifar10.py
@@ -56,7 +56,7 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 18, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
         self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
