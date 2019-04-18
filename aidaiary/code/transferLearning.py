868a7d332e159e4356f74c1e1add6d5970e24611
fuchami <famichiki.yuuki@gmail.com>
2019-04-18 16:26:38 +0900

    fix

--- aidaiary/code/transferLearning.py
+++ aidaiary/code/transferLearning.py
@@ -54,7 +54,7 @@ data_transforms = {
         transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(256,256),
+        transforms.Resize((256,256)),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225])
